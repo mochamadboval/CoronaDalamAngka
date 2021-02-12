e68469bcf08cb34878836963ee8bcd8ab310ef2f
mochamadboval <mbovalap@gmail.com>
2021-02-12 14:41:57 +0700

    Menambahkan rel="noreferrer"

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,23 +4,23 @@
     <small>
       <fa-icon icon="code" /> menggunakan 
       <fa-icon :icon="['fab', 'html5']" /> <fa-icon :icon="['fab', 'css3-alt']" /> <fa-icon :icon="['fab', 'js-square']" /> <fa-icon :icon="['fab', 'bootstrap']" /> <fa-icon :icon="['fab', 'vuejs']" /> <fa-icon :icon="['fab', 'font-awesome']" />
-      oleh <strong><a href="https://boval.my.id/" target="_blank">Mochamad Boval</a></strong>
+      oleh <strong><a href="https://boval.my.id/" rel="noreferrer" target="_blank">Mochamad Boval</a></strong>
       <br>
       Sumber Data dari 
       <strong>
-        <a href="https://covid19.go.id/" target="_blank">
+        <a href="https://covid19.go.id/" rel="noreferrer" target="_blank">
           Satgas Penanganan COVID-19
         </a>
       </strong> melalui 
       <strong>
-        <a href="https://github.com/Reynadi531/api-covid19-indonesia-v2" target="_blank">
+        <a href="https://github.com/Reynadi531/api-covid19-indonesia-v2" rel="noreferrer" target="_blank">
           Reynadi531 API
         </a>
       </strong>
       <br>
       Sumber Kode di 
       <strong>
-        <a href="https://github.com/mochamadboval?tab=repositories" target="_blank">
+        <a href="https://github.com/mochamadboval?tab=repositories" rel="noreferrer" target="_blank">
           <fa-icon :icon="['fab', 'github']" /> Github
         </a>
       </strong>

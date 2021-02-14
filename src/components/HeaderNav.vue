cec4617091e62acd48f7d98d16d836e33cca44c9
mochamadboval <mbovalap@gmail.com>
2021-02-14 07:58:23 +0700

    Menambahkan aria-label="Menu Navigasi" pada Button

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -2,7 +2,7 @@
   <nav class="navbar navbar-expand-sm fixed-top navbar-dark bg-dark shadow">
     <div class="container">
       <a class="navbar-brand" href="#"><fa-icon icon="virus" /> Corona Dalam Angka</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#Menu">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#Menu" aria-label="Menu Navigasi">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse justify-content-end" id="Menu">
